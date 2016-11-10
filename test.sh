80256e418e418de1f6a2a04904d4c709f8bcb924
Andrey Smetanin <irqlevel@gmail.com>
2016-11-11 00:58:11 +0300

    change cred

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ WDIR=temp
 
 echo 1 > /sys/fs/vdisk/create_session
 echo 127.0.0.1:9111 > /sys/fs/vdisk/session1/connect
-echo a@b.com 1q2w3e > /sys/fs/vdisk/session1/login
+echo a@b.com 1q > /sys/fs/vdisk/session1/login
 echo 1 > /sys/fs/vdisk/session1/logout
 echo 1 > /sys/fs/vdisk/session1/disconnect
 echo 1 > /sys/fs/vdisk/delete_session 
